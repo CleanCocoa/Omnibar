915b28e319b5d11625a32cbcc464fb4491f17c60
Christian Tietze <christian.tietze@gmail.com>
2017-05-31 07:43:16 +0200

    Initial Commit

---
+++ Omnibar/Omnibar.h
@@ -0,0 +1,19 @@
+//
+//  Omnibar.h
+//  Omnibar
+//
+//  Created by Christian Tietze on 31/05/17.
+//  Copyright © 2017 Christian Tietze. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Omnibar.
+FOUNDATION_EXPORT double OmnibarVersionNumber;
+
+//! Project version string for Omnibar.
+FOUNDATION_EXPORT const unsigned char OmnibarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Omnibar/PublicHeader.h>
+
+
