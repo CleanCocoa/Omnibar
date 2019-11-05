7e1c412c762ec3fdf206b068029ef4127fd39244
Christian Tietze <christian.tietze@gmail.com>
2019-11-05 08:42:14 +0100

    replace deprecated `E` with `Element`

--- RxExample/RxHelpers.swift
+++ RxExample/RxHelpers.swift
@@ -23,8 +23,8 @@ extension Optional: OptionalType {
 import RxSwift
 
 /// via <https://github.com/RxSugar/RxSugar/tree/d639d4279f8f465c16aff3618d7c503da3dfe3ce/RxSugar/IgnoreNil.swift>
-extension ObservableType where E: OptionalType {
-    public func ignoreNil() -> Observable<E.Wrapped> {
+extension ObservableType where Element: OptionalType {
+    public func ignoreNil() -> Observable<Element.Wrapped> {
         return filter { $0.hasValue() }
             .map { $0.optional! }
     }
@@ -33,13 +33,13 @@ extension ObservableType where E: OptionalType {
 import RxCocoa
 
 /// via <https://github.com/RxSwiftCommunity/RxOptional/tree/93d12b3c3bfc1886ebd038149b24a8d7d6ef1ea4/Source/Driver%2BOptional.swift>
-extension SharedSequenceConvertibleType where SharingStrategy == DriverSharingStrategy, E: OptionalType {
-    public func ignoreNil() -> Driver<E.Wrapped> {
-        return self.flatMap { element -> Driver<E.Wrapped> in
+extension SharedSequenceConvertibleType where SharingStrategy == DriverSharingStrategy, Element: OptionalType {
+    public func ignoreNil() -> Driver<Element.Wrapped> {
+        return self.flatMap { element -> Driver<Element.Wrapped> in
             guard let value = element.optional else {
-                return Driver<E.Wrapped>.empty()
+                return Driver<Element.Wrapped>.empty()
             }
-            return Driver<E.Wrapped>.just(value)
+            return Driver<Element.Wrapped>.just(value)
         }
     }
 }
